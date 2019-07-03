b2a0fcda15ee5a68618fdbadf8532556c561f003
rodolk <rodolk@yahoo.com>
2019-07-03 06:59:55 -0700

    Added statistics

--- src/main/java/com/rodolk/numserver/logserver/IntProcessor.java
+++ src/main/java/com/rodolk/numserver/logserver/IntProcessor.java
@@ -3,54 +3,59 @@ package com.rodolk.numserver.logserver;
 import com.rodolk.numserver.loggingprotocol.ArrayProvider;
 
 public class IntProcessor implements Runnable {
-	BufferQueue inputQueue_;
-	BufferQueue outputQueue_;
-	ArrayProvider inputArrayProvider_;
-	ArrayProvider outputArrayProvider_;
-	IntStorageStrategy intStorage_;
-	Boolean terminate_ = false;
-	
-	public IntProcessor(BufferQueue inQueue, BufferQueue outQueue, ArrayProvider in, 
-			ArrayProvider out, IntStorageStrategy intStorage) {
-		inputQueue_ = inQueue;
-		outputQueue_ = outQueue;
-		inputArrayProvider_ = in; 
-		outputArrayProvider_ = out;
-		intStorage_ = intStorage;
-	}
-	
-	public void setTerminate() {
-		terminate_ = true;
-	}
-	
-	@Override
-	public void run() {
-		while(!terminate_) {
-			ArrayProvider.ArrayElement arrayElemOut = outputArrayProvider_.getArray();
-			if (arrayElemOut != null) {
-				ArrayProvider.ArrayElement arrayElemIn = inputQueue_.poll();
-				if (arrayElemIn != null) {
-					int numIdx;
-					int outNum = 0;
-					for(numIdx = 0; numIdx < arrayElemIn.totalNumbers_; numIdx++) {
-						boolean contains = intStorage_.contains(arrayElemIn.array_, 
-										  (arrayElemIn.initialPos_ + (numIdx * arrayElemIn.numberLen_)), 
-										  arrayElemIn.numberLen_);
-						if (!contains) {
-							for(int j = 0; j < arrayElemIn.numberLen_; j++) {
-								arrayElemOut.array_[(outNum * arrayElemIn.numberLen_) + j] = 
-									arrayElemIn.array_[(arrayElemIn.initialPos_ + (numIdx * arrayElemIn.numberLen_)) + j];
-							}
-							outNum++;
-						}
-					}
-					arrayElemOut.setArrayValues(0, outNum, 10);
-					outputQueue_.add(arrayElemOut);
-					inputArrayProvider_.returnArray(arrayElemIn);
-				} else {
-					outputArrayProvider_.returnArray(arrayElemOut);
-				}
-			}
-		}
-	}
+    BufferQueue inputQueue_;
+    BufferQueue outputQueue_;
+    ArrayProvider inputArrayProvider_;
+    ArrayProvider outputArrayProvider_;
+    IntStorageStrategy intStorage_;
+    Boolean terminate_ = false;
+    Statistics.StatisticsCounter statsCounter_;
+    
+    public IntProcessor(BufferQueue inQueue, BufferQueue outQueue, ArrayProvider in, 
+            ArrayProvider out, IntStorageStrategy intStorage, Statistics.StatisticsCounter statsCounter) {
+        inputQueue_ = inQueue;
+        outputQueue_ = outQueue;
+        inputArrayProvider_ = in; 
+        outputArrayProvider_ = out;
+        intStorage_ = intStorage;
+        statsCounter_ = statsCounter;
+    }
+    
+    public void setTerminate() {
+        terminate_ = true;
+    }
+    
+    @Override
+    public void run() {
+        while(!terminate_) {
+            ArrayProvider.ArrayElement arrayElemOut = outputArrayProvider_.getArray();
+            if (arrayElemOut != null) {
+                ArrayProvider.ArrayElement arrayElemIn = inputQueue_.poll();
+                if (arrayElemIn != null) {
+                    int numIdx;
+                    int outNum = 0;
+                    for(numIdx = 0; numIdx < arrayElemIn.totalNumbers_; numIdx++) {
+                        boolean contains = intStorage_.contains(arrayElemIn.array_, 
+                                          (arrayElemIn.initialPos_ + (numIdx * arrayElemIn.numberLen_)), 
+                                          arrayElemIn.numberLen_);
+                        if (!contains) {
+                            for(int j = 0; j < arrayElemIn.numberLen_; j++) {
+                                arrayElemOut.array_[(outNum * arrayElemIn.numberLen_) + j] = 
+                                    arrayElemIn.array_[(arrayElemIn.initialPos_ + (numIdx * arrayElemIn.numberLen_)) + j];
+                            }
+                            statsCounter_.incUnique();
+                            outNum++;
+                        } else {
+                            statsCounter_.incDuplicate();
+                        }
+                    }
+                    arrayElemOut.setArrayValues(0, outNum, 10);
+                    outputQueue_.add(arrayElemOut);
+                    inputArrayProvider_.returnArray(arrayElemIn);
+                } else {
+                    outputArrayProvider_.returnArray(arrayElemOut);
+                }
+            }
+        }
+    }
 }
